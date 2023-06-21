856c6e9fd3a14acd8af8ea7d7eef394372bf155f
CybersecurityDev <cybersecurityweb@ar.mcd.com>
2023-06-21 12:31:48 -0300

    se corrige validación de form

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@ let totalAPagar = 0;
 
 const resumen = document.getElementById("btnResumen").addEventListener("click", precioFinal);
 
-function precioFinal() {
-
+function precioFinal(event) {
+   
     let categoria = document.getElementById("categoria").value;
     if (categoria == "1") {
         valorTicketFinal = valorTicket - (valorTicket * descEstudiante);
@@ -19,17 +19,25 @@ function precioFinal() {
         valorTicketFinal = valorTicket - (valorTicket * descJunior);
     } else {
         valorTicketFinal = valorTicket;
-    }
+    };
 
     let cantidadTickets = document.getElementById("cantidad").value;
     cantidadTickets = parseInt(cantidadTickets);
-    
     totalAPagar = cantidadTickets * valorTicketFinal;
+    
+    if (totalAPagar) { 
+        event.preventDefault();
+        const total = document.getElementById("total");
+        total.setAttribute("value", `Total a Pagar: $${totalAPagar}`);
+        total.innerHTML = total.getAttribute('value');
+    };
 
-    const total = document.getElementById("total");
+};
 
-    total.setAttribute("value", `Total a pagar: $${totalAPagar}`);
-
+const borrarDatos = document.getElementById("btnBorrar").addEventListener("click", borrar);
+function borrar() {
+   
+    total.setAttribute("value", `Total a Pagar: $`);
     total.innerHTML = total.getAttribute('value');
 
-}
+};
